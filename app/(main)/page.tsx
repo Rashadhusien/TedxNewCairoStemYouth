b4640f119bc2fb34912d3375e62e4d82d62e7179
rashad <rashadhusein440@gmail.com>
2026-06-10 16:45:18 +0300

    remove offer section from home

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,25 +3,11 @@ import Hero from "./components/hero-section";
 import SpeakersSection from "./components/speakers-section";
 import CountdownTimer from "./components/countdown-timer";
 import SponsorsSection from "./components/sponsors-section";
-import OffersBanner from "@/components/tickets/offers-banner";
-import { getActiveOffers } from "@/lib/db/actions/offer.action";
 
-const Home = async () => {
-  const offersResult = await getActiveOffers(true);
-  const featuredOffers =
-    offersResult.success && offersResult.data ? offersResult.data : [];
-
-
+const Home = () => {
   return (
     <div className="">
-    
       <Hero />
-
-      {featuredOffers.length > 0 && (
-        <section className="relative py-12 px-6 max-w-6xl mx-auto">
-          <OffersBanner offers={featuredOffers} />
-        </section>
-      )}
 
       <AboutSection />
 
